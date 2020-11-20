3f22941a02679bba0daa05c7d9be66cd9bb6f7d8
Dmitri Maksimov <dmitri.maksimov@gtcybertech.com>
2020-11-20 15:52:30 +0200

    chore: change Gradle build files to use Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+  repositories {
+    google()
+    jcenter()
+  }
+  dependencies {
+    classpath("com.android.tools.build:gradle:4.1.0")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+
+    // NOTE: Do not place your application dependencies here; they belong
+    // in the individual module build.gradle files
+  }
+}
+
+allprojects {
+  repositories {
+    google()
+    jcenter()
+  }
+}
+
+tasks.register<Delete>("clean") {
+  delete = setOf(rootProject.buildDir)
+}
+
+tasks.withType<Wrapper> {
+  gradleVersion = "6.7"
+  distributionType = Wrapper.DistributionType.BIN
+}
